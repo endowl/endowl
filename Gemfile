1664359931d75192fcc0115c35fcf078de762d50
morgan <morganstar@gmail.com>
2020-11-09 19:25:53 -0800

    Implement existing Endowl theme

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem "github-pages", group: :jekyll_plugins
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.12"
   gem "jekyll-seo-tag", "~> 2.6"
+  gem "jekyll-paginate"
 end
 
 # Windows and JRuby does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ letter-to-heirs/precache-manifest.85712adcf422908a741b04f87afc3c38.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e11f9a20da6505dadef3ea6ca28084",
+    "url": "/letter-to-heirs/index.html"
+  },
+  {
+    "revision": "ee2083447796d1c3ed0c",
+    "url": "/letter-to-heirs/static/css/3.2a86faee.chunk.css"
+  },
+  {
+    "revision": "21519552d5c0c1c5498f",
+    "url": "/letter-to-heirs/static/css/main.e144cf91.chunk.css"
+  },
+  {
+    "revision": "1ed293b9e9b83a353a7c",
+    "url": "/letter-to-heirs/static/js/2.7c89ead7.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/letter-to-heirs/static/js/2.7c89ead7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2083447796d1c3ed0c",
+    "url": "/letter-to-heirs/static/js/3.65cba93f.chunk.js"
+  },
+  {
+    "revision": "4f421e98da370aa7166e18e6c5770c97",
+    "url": "/letter-to-heirs/static/js/3.65cba93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46fd598be32434ad0659",
+    "url": "/letter-to-heirs/static/js/4.6ca0da23.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/letter-to-heirs/static/js/4.6ca0da23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21519552d5c0c1c5498f",
+    "url": "/letter-to-heirs/static/js/main.5bed0377.chunk.js"
+  },
+  {
+    "revision": "6973f0479f68876c449c",
+    "url": "/letter-to-heirs/static/js/runtime-main.5064ef4a.js"
+  },
+  {
+    "revision": "6183e7e4ca884514174d77975d0e61b0",
+    "url": "/letter-to-heirs/static/media/moroccan-flower.6183e7e4.png"
+  }
+]);
