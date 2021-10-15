a0ad4ffb184415a4caf68320e0b33800cef843c0
Aaron Anderson <aaronlynanderson@gmail.com>
2021-10-15 11:47:44 -0500

    Trying to fix production routing

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Main from './components/Main';
+import Glossary from './pages/Glossary';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
+import Share from './pages/Share';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route }  from 'react-router-dom';
 import smoothScrollPolyfill from 'smoothscroll-polyfill';
 
 smoothScrollPolyfill.polyfill();
@@ -11,7 +16,15 @@ smoothScrollPolyfill.polyfill();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App/>
+      <App>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/glossary" component={Glossary} />
+          <Route exact path="/privacy" component={Privacy} />
+          <Route exact path="/terms" component={Terms} />
+          <Route exact path="/post-interview-share" component={Share} />
+        </Switch>
+      </App>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
